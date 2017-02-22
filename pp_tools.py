566afe45bd2bb8ec12c789f8faa4590294cf243b
Dexter Sealy <dextersealy@gmail.com>
2017-02-21 23:22:07 -0500

    add nrows kwarg to pp_dict

--- pp_tools.py
+++ pp_tools.py
@@ -1,9 +1,31 @@
 from IPython.display import display, HTML
 
-def pp_dict(d):
+def pp_bold(str):
+    display(HTML('<b>{}</b>'.format(str)))
+
+def pp_listOflist(l):
     display(HTML(
         u'<table>{}</table>'.format(
-            u''.join(u'<tr><td><b>{}</b></td><td>{}</td></tr>'.format(k, d[k]) for k in d))))
+            u''.join(u'<tr>{}</tr>'.format(
+                u''.join(u'<td>{}</td>'.format(v) for v in sublist)) for sublist in l))))
+    
+def pp_dict(d, rows=None):
+    if not rows or rows >= len(d):
+        display(HTML(
+            u'<table>{}</table>'.format(
+                u''.join(u'<tr><td><b>{}</b></td><td>{}</td></tr>'.format(k, d[k]) for k in d))))
+    else:
+        nitems = len(d)
+        width = -(-nitems // rows)
+        i = 0
+        list_ = [[] for _ in range(rows)]
+        for _ in range(width):
+            for row in range(rows):
+                if i < nitems:
+                    k, v = d.items()[i]
+                    list_[row].extend(['<b>{}</b>'.format(k), v])
+                i += 1
+        pp_listOflist(list_)
 
 def pp_dictOflist(d):
     display(HTML(
@@ -11,15 +33,6 @@ def pp_dictOflist(d):
             u''.join(u'<tr><td><b>{}</b></td>{}</tr>'.format(k,
                 u''.join(u'<td>{}</td>'.format(v) for v in d[k])) for k in d.keys()))))
     
-def pp_listOflist(l):
-    display(HTML(
-        u'<table>{}</table>'.format(
-            u''.join(u'<tr>{}</tr>'.format(
-                u''.join(u'<td>{}</td>'.format(v) for v in sublist)) for sublist in l))))
-    
-def pp_bold(str):
-    display(HTML('<b>{}</b>'.format(str)))
-
 def pp_dfinfo(df, width=4):
     ncols = len(df.columns)
     width = min(width, ncols)
